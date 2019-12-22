572da5f62c21e2b335e208557e1e1e392e79e6e6
David Weis <davidmicahelweis@gmail.com>
2019-12-22 01:47:30 +0000

    Add unfinished day 12 task 2

--- src/day_twelve.rs
+++ src/day_twelve.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::ops::Add;
 use std::fmt;
+use std::collections::{HashMap, HashSet};
 
 fn get_input() -> String {
     fs::read_to_string("input/day_twelve.txt")
@@ -26,7 +27,7 @@ fn parse_input(text: &str) -> Vec<Moon> {
     moons
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 struct Vector {
     x: i32,
     y: i32,
@@ -55,7 +56,7 @@ impl Add<&Vector> for &Vector {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 struct Moon {
     position: Vector,
     velocity: Vector
@@ -120,13 +121,73 @@ fn simulate_moons(moons: &Vec<Moon>, steps: i32) -> Vec<Moon> {
     moons
 }
 
-pub fn one(){
+fn simulate_axis_until_repeat(moons: &Vec<Moon>) -> i32 {
+    let mut previous: HashMap<usize, HashMap<usize, HashSet<i32>>> = HashMap::new();
+    let mut oscilation: HashMap<usize, HashMap<usize, Option<i32>>> = HashMap::new();
+    let mut counter = 0;
+    let mut moons = moons.clone();
+    for i in 0..4 {
+        let mut local_map: HashMap<usize, HashSet<i32>> = HashMap::new();
+        let mut local_osc: HashMap<usize, Option<i32>> = HashMap::new();
+        for o in 0..3 {
+            local_map.insert(o, HashSet::new());
+            local_osc.insert(o, None);
+        }
+        previous.insert(i, local_map);
+        oscilation.insert(i, local_osc);
+    }
+    loop {
+        counter += 1;
+        let mut new_moons = moons.clone();
+        for (i, current_moon) in new_moons.iter_mut().enumerate() {
+            // adjust velocities
+            for other_moon in moons.iter() {
+                if current_moon.position.x > other_moon.position.x {
+                    current_moon.velocity.x -= 1
+                } else if current_moon.position.x < other_moon.position.x {
+                    current_moon.velocity.x += 1
+                }
+                if current_moon.position.y > other_moon.position.y {
+                    current_moon.velocity.y -= 1
+                } else if current_moon.position.y < other_moon.position.y {
+                    current_moon.velocity.y += 1
+                }
+                if current_moon.position.z > other_moon.position.z {
+                    current_moon.velocity.z -= 1
+                } else if current_moon.position.z < other_moon.position.z {
+                    current_moon.velocity.z += 1
+                }
+            }
+            // adjust position
+            current_moon.position = &current_moon.position + &current_moon.velocity;
+            if oscilation.get(&i).unwrap().get(&0).unwrap().is_none() {
+                // let mut set = previous.get_mut(&i).unwrap().get_mut(&0).unwrap();
+                // if 
+            }
+        }
+        if counter % 1000000 == 0 {
+            println!("{}", counter)
+        }
+        moons = new_moons;
+    }
+}
+
+pub fn one() {
     let text = get_input();
     let moons = parse_input(&text);
     let moons = simulate_moons(&moons, 1000);
     let system_energy: i32 = moons.iter().map(|moon| moon.total_energy()).sum();
     println!("{}", system_energy)
 }
+
+// pub fn two() {
+//     let moons = parse_input("<x=-8, y=-10, z=0>
+//     <x=5, y=5, z=10>
+//     <x=2, y=-7, z=3>
+//     <x=9, y=-8, z=-3>");
+//     // let iter = simulate_until_repeat(&moons);
+//     // println!("{}", iter);
+// }
 
 #[cfg(test)]
 mod tests {
